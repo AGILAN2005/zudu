06faae4ea9ea0ad4d6951b34989e419c57fd1b0d
AGILAN2005 <agilan991122@gmail.com>
2025-10-26 11:00:45 +0530

    testing with edge cases

--- test.py
+++ test.py
@@ -20,23 +20,9 @@ if __name__ == "__main__":
         print(f"Ingested {num_chunks} chunks.")
 
         questions = [
-            "What is the fundamental difference in traffic (organic vs. paid) and one corresponding success metric for MakeMyTrip and OYO Rooms?",
-            "How did the company's content marketing address the challenge of 'Complex Product Understanding' in the insurance sector?",
-            "How do personalized recommendations (using predictive analytics) and campaign optimization enhance both customer experience and ROI?",
-            "Identify the specific campaign that served as a core example of both 'Emotional Branding' and the company's 'Social Responsibility.'",
-            "What keyword strategy did both MakeMyTrip (SEO) and OYO Rooms (SEM) use to reduce competition and advertising costs, and why is it effective for both?",
-            "What is the Core Product of a smartphone, and which vehicle did Tata Motors position for the ''people's car'' segment?",
-            "What four dimensions define a company's Product Mix, and which two main categories did HUL use to achieve its wide market share?",
-            "How did Marico's Product Differentiation strategy for Parachute and Saffola specifically address the challenge of 'Changing Consumer Preferences'?",
-            "What is the primary objective of Integrated Marketing Communication (IMC), and what unifying mascot did Amul use to maintain consistency?",
-            "What Differentiation Strategy did Royal Enfield use by organizing events like Rider Mania, and which two types of CRM did HDFC Bank apply?",
-            "What was the primary solution ITC implemented to address the challenge of 'Supply Chain Disruptions,' and which technological investment supported this?",
-            "Beyond product launches, how does ITC's 'Sustainability and CSR Monitoring' contribute to its competitive edge and stakeholder relationships?",
-            "what is the difference between what 'Continuous Monitoring' helps a company reduce, and what 'Adaptability' is essential for maintaining?",
-            "besides Historical Data Analysis, name the two other categories of sophisticated models Maruti Suzuki uses to predict demand accurately.",
-            "State the approximate percentage by which Maruti Suzuki reduced forecast errors and the separate percentage reduction achieved in excess inventory costs.",
-            "what is the two-part focus of Maruti Suzuki's future outlook regarding new product lines and new markets?",
-
+            "Jio disrupted a large market. What is the technical term for a market dominated by just two major competitors?",
+            "What does the acronym 'URL' stand for, and what language is used for defining the structure of web pages?",
+            "What term describes the market state where quantity supplied equals quantity demanded?"
         ]
 
         for question in questions:
